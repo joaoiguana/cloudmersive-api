6b8d3f5f5a634c55f91b1d064d625b01402af468
Joao Iguana <jiboiadamor@gmail.com>
2024-06-14 22:32:14 +0100

    created an initializer for cloudmersive

---
+++ config/initializers/cloudmersive.rb
@@ -0,0 +1,10 @@
+# Load the gem
+require 'cloudmersive-convert-api-client'
+
+# Setup authorization
+CloudmersiveConvertApiClient.configure do |config|
+  # Configure API key authorization: Apikey
+  config.api_key['Apikey'] = ENV['CLOUDMERSIVE_API_KEY']
+  # Uncomment the following line to set a prefix for the API key, e.g. 'Bearer' (defaults to nil)
+  # config.api_key_prefix['Apikey'] = 'Bearer'
+end
